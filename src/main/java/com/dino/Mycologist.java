9e43279283ed770a5dc991ffe7ab4bea27782adb
Hujber Hunor <hujber.hunor@gmail.com>
2025-03-21 09:40:04 +0100

    Mychologist visszacsinálva. Vidi megcsinálta

--- src/main/java/com/dino/Mycologist.java
+++ src/main/java/com/dino/Mycologist.java
@@ -1,14 +1,4 @@
 package com.dino;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * A Player osztály leszármazottja, egy gombászt reprezentál. Megvalósítja a gombászokra specifikus olyan akciót,
- * ami nem egy specifikus gombához tartozik, hanem magához a játékoshoz, illetve számontartja a játékos gombatestjeit.
- */
-public class Mycologist extends Player {
-
-    private List<Fungus> fungi = new ArrayList<>();
-    // public boolean placeFungus(Tecton t);
-} // End of Mychoogist
+public class Mycologist {
+}
