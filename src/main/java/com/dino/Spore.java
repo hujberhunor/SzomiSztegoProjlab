0e1ba0cec4f799567f71c92b95af89e90d081eeb
Kovács Levente <kovente2003@gmail.com>
2025-03-21 21:38:07 +0100

    Demi-progress

--- src/main/java/com/dino/Spore.java
+++ src/main/java/com/dino/Spore.java
@@ -5,9 +5,18 @@ public abstract class Spore {
     protected int effectDuration;
     protected int nutrientValue;
 
-    public void move() { }
-    public void eat() { }
-    public void cut() { }
+    //public boolean move(Tecton t)?
+    public void move() {
+        System.out.println("Spore.move()");
+    }
+    //public boolean eat(Entomologist e)?
+    public void eat() {
+        System.out.println("Spore.eat()");
+    }
+    //public boolean cut(Tecton t, Hypha h)?
+    public void cut() {
+        System.out.println("Spore.cut()");
+    }
     public void update() { }
     public abstract void applyTo(Insect i);
 }
