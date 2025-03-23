a3bff28a1785f131212e28e813117ea2558218ba
Hujber Hunor <hujber.hunor@gmail.com>
2025-03-23 21:33:53 +0100

    getTectons duplicate resolved

--- src/main/java/com/dino/Hypha.java
+++ src/main/java/com/dino/Hypha.java
@@ -54,8 +54,4 @@ public class Hypha {
     public void continueHypha(Tecton t) {}
 
     public void continueHypha(Hypha h) {}
-
-    public List<Tecton> getTectons(){
-        return tectons;
-    }
 } // End of Hypha
