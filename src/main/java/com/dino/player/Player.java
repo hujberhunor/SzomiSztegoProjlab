876742744d8ebffc630116ebd43ad37ff31126f6
Hujber Hunor <hujber.hunor@gmail.com>
2025-04-23 14:46:09 +0200

    feat: Player serialize method

--- src/main/java/com/dino/player/Player.java
+++ src/main/java/com/dino/player/Player.java
@@ -1,10 +1,13 @@
 package com.dino.player;
 
 import com.dino.tecton.Tecton;
+import com.google.gson.JsonObject;
 
 /**
- * Ez az osztály alapvető, minden játékos által használható cselekvéseket valósítja meg,
- * illetve egy olyan tulajdonságot tárol, ami mind gombászokra, mind rovarászokra egyaránt értelmezhető.
+ * Ez az osztály alapvető, minden játékos által használható cselekvéseket
+ * valósítja meg,
+ * illetve egy olyan tulajdonságot tárol, ami mind gombászokra, mind
+ * rovarászokra egyaránt értelmezhető.
  */
 public abstract class Player {
     /**
@@ -18,18 +21,27 @@ public abstract class Player {
     public int score;
 
     /**
-     *  Egy egész szám, ami megadja, hogy a játékos hány akcióval kezdi a körét.
-     *  Értéke gombászok és rovarászok között eltérő.
+     * Egy egész szám, ami megadja, hogy a játékos hány akcióval kezdi a körét.
+     * Értéke gombászok és rovarászok között eltérő.
      */
     public int actionsPerTurn;
 
     /**
-     * Egy egész szám, ami megadja, hogy a játékosnak hány akciója van hátra a körében.
+     * Egy egész szám, ami megadja, hogy a játékosnak hány akciója van hátra a
+     * körében.
      */
     public int remainingActions;
 
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+        this.actionsPerTurn = 3; // alapértelmezett érték, pl. rovarásznak
+        this.remainingActions = 3;
+    }
+
     /**
      * Visszaad egy kivánt tektont.
+     * 
      * @param tecton A kiválasztotandó tekton.
      * @return A kiválasztott tekton.
      */
@@ -39,9 +51,20 @@ public abstract class Player {
 
     /**
      * Visszaadja a pontszám értékét.
+     * 
      * @return A játékos aktuális pontszáma.
      */
     public int calculateScore() {
         return score;
     }
+
+    public JsonObject serialize() {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("name", this.name);
+        obj.addProperty("score", this.score);
+        obj.addProperty("actionsPerTurn", this.actionsPerTurn);
+        obj.addProperty("remainingActions", this.remainingActions);
+        return obj;
+    }
+
 }
