75978dfcc5729e8696056b1411060c50ccd8030d
Szabó Marcell István <szabo.marcell.istvan@gmail.com>
2025-04-24 12:48:53 +0200

    SingleHyphaTecton megvalósítása

--- src/main/java/com/dino/tecton/SingleHyphaTecton.java
+++ src/main/java/com/dino/tecton/SingleHyphaTecton.java
@@ -6,7 +6,8 @@ import com.dino.core.Hypha;
  * Egy olyan konkrét, példányosítható tektontípus, amin egyszerre csak egy gombafonál lehet jelen.
  */
 public class SingleHyphaTecton extends Tecton {
-    private int hyphaLimit; 
+    private int hyphaLimit;
+    
     /**
      * Konstruktor
      */
@@ -17,14 +18,41 @@ public class SingleHyphaTecton extends Tecton {
     
     /**
      * A fonalakat kezelő virtuális függvény felülírása.
+     * Ellenőrzi, hogy van-e már gombafonál a tektonon. Ha nincs,
+     * hozzáadja az újat. Ha már van egy fonál, eltávolítja azt és
+     * helyére teszi az újat.
      * 
      * @param h A kezelendő gombafonál
      */
     @Override
     public void handleHypha(Hypha h) {
-        //TODO
+        if (hyphas.isEmpty()) {
+            // Ha még nincs fonál, egyszerűen hozzáadjuk az újat
+            hyphas.add(h);
+        } else {
+            // Ha már van fonál, lecseréljük az újra
+            Hypha existingHypha = hyphas.get(0);
+            hyphas.clear();
+            hyphas.add(h);
+            
+            // Itt lehetne kód, ami bejelenti a Game osztálynak, hogy egy fonál eltűnt
+        }
     }
     
+    /**
+     * Visszaadja a tektonon megengedett fonalak számát
+     * 
+     * @return Mindig 1, mivel ez egy SingleHyphaTecton
+     */
+    public int getHyphaLimit() {
+        return hyphaLimit;
+    }
+    
+    /**
+     * Létrehoz egy új, ugyanolyan típusú tektont
+     * 
+     * @return Új SingleHyphaTecton példány
+     */
     @Override
     public Tecton createCopy() {
         return new SingleHyphaTecton();
